a8b525c21b2d9631fb7cfefa450ee7dafbd4ef55
Oscar <oddpaz7@gmail.com>
2025-11-09 22:03:05 -0500

    BD added on Users now it shows and save on the BD

---
+++ GUI/Program.cs
@@ -0,0 +1,60 @@
+using Blazorise;
+using Blazorise.Bootstrap5;
+using Blazorise.Icons.FontAwesome;
+using BLL.Implementaciones;
+using BLL.Interfaces;
+using DAL.Implementaciones;
+using DAL.Interfaces;
+using GUI.Components;
+using Oracle.ManagedDataAccess.Client;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddBlazorise(options =>
+    {
+        options.Immediate = true;
+    })
+    .AddBootstrap5Providers()
+    .AddFontAwesomeIcons();
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+// Configurar Wallet PRIMERO, antes de cualquier conexión
+var walletLocation = builder.Configuration["OracleWallet:Location"]!;
+OracleConfiguration.TnsAdmin = walletLocation;
+OracleConfiguration.WalletLocation = walletLocation;
+
+// Obtener la cadena de conexión
+var connectionString = builder.Configuration.GetConnectionString("OracleConnection")!;
+
+// ===== REGISTRAR SERVICIOS DE LA CAPA DAL =====
+builder.Services.AddScoped<IUsuarioDAO>(sp => new UsuarioDAO(connectionString));
+builder.Services.AddScoped<IRolDAO>(sp => new RolDAO(connectionString));
+
+// ===== REGISTRAR SERVICIOS DE LA CAPA BLL =====
+builder.Services.AddScoped<IUsuarioService, UsuarioService>();
+builder.Services.AddScoped<IRolService, RolService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
