6565dd0c0c025fe5975e33a47d6cc88e91ad0fd1
Oscar <oddpaz7@gmail.com>
2025-11-12 01:08:17 -0500

    Design changes made on project

--- ENTITY/Usuarios/UsuarioDTO.cs
+++ ENTITY/Usuarios/UsuarioDTO.cs
@@ -10,27 +10,27 @@ namespace ENTITY.Usuarios
     public class UsuarioDTO
     {
         public int Id { get; set; }
-        public int RolId { get; set; }      // Siempre 1 o 2 (ADMIN o CLIENTE)
-        public int? DireccionId { get; set; }  // FK opcional (usuario recién creado puede llenarla luego)
-
+        public int RolId { get; set; }
+        public int? DireccionId { get; set; }
         public string Cedula { get; set; } = null!;
         public string PrimerNombre { get; set; } = null!;
         public string? SegundoNombre { get; set; }
         public string ApellidoPaterno { get; set; } = null!;
         public string? ApellidoMaterno { get; set; }
-
         public string TelefonoPrincipal { get; set; } = null!;
         public string? TelefonoSecundario { get; set; }
-
         public string Correo { get; set; } = null!;
         public string Contrasena { get; set; } = null!;
-
         public string Estado { get; set; } = "A";
         public DateTime FechaRegistro { get; set; }
 
         // Relaciones
         public DireccionDTO? Direccion { get; set; }
         public RolDTO? Rol { get; set; }
-    }
 
+        public string ObtenerNombreCompleto()
+        {
+            return $"{PrimerNombre} {SegundoNombre} {ApellidoPaterno} {ApellidoMaterno}".Replace("  ", " ").Trim();
+        }
+    }
 }
